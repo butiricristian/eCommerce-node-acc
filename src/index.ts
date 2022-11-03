e2efb97b9ec36c0c38f2298605b7499acaa08d0d
cristian_butiri <cristian.butiri@andopen.co>
2022-11-04 01:19:34 +0200

    Add mongo config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import * as express from 'express';
+import * as debug from 'debug'
+import { connectDatabase } from './common/mongoose.service';
 const PORT = 3001;
 
 const app: express.Application = express();
+const log = debug('app:main')
+
 app.use(express.json())
+connectDatabase()
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  return console.log(`Express is listening at http://localhost:${PORT}`);
+  return log(`Express is listening at http://localhost:${PORT}`);
 });
