c8482a3cdbf451b23d4e1c9d9afedd9973c14ad0
cristian_butiri <cristian.butiri@andopen.co>
2022-11-17 11:27:29 +0200

    add and configure swagger

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import * as express from 'express';
 import * as debug from 'debug'
 import { connectDatabase } from './common/mongoose.service';
+import { SwaggerConfig } from './swagger.config';
 const PORT = 3001;
 
 const app: express.Application = express();
 const log = debug('app:main')
 
+
 app.use(express.json())
 connectDatabase()
 
+/**
+ * Default home path
+ */
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+
+new SwaggerConfig(app, 'localhost', PORT);
 
 app.listen(PORT, () => {
   return log(`Express is listening at http://localhost:${PORT}`);
